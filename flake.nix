54fa7e7bcdbb37551095a2abbc0fdd09c07d5379
Nazar Vinnichuk <nazar@vinnich.uk>
2022-11-17 21:25:32 +0200

    Add flake.nix.

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; };
+    {
+      devShell = mkShell {
+        packages = [
+          jdk17_headless
+        ];
+      };
+    }
+  );
+}
